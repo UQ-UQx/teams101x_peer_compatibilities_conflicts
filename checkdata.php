2840e15d10759a6cab356acdcac28b05da8579e5
Ankith Konda <a.konda@uq.edu.au>
2016-03-14 01:48:16 +1000

    Add form and check submission functionality

--- checkdata.php
+++ checkdata.php
@@ -8,62 +8,120 @@
 	
 	$vars = array('user_id'=>$_POST['user_id'],'oauth_consumer_key'=>$_POST['lti_id'], 'lis_outcome_service_url'=>$_POST['lis_outcome_service_url'], 'lis_result_sourcedid'=>$_POST['lis_result_sourcedid']);
 	
-	
 	$answer_key_string = '{
 
 			"roles":{
 
 				"Implementer":{
-					"compatible":["Resource Investigator", "Monitor Evaluator", "Coordinator"],
+					"compatible":["Resource_Investigator", "Monitor", "Coordinator"],
 					"conflict":["Implementer", "Plant"]
 				},
 
 				"Plant":{
-					"compatible":["Coordinator", "Team Worker", "Resource Investigator"],
-					"conflict":["Plant", "Implementer", "Monitor Evaluator"]
+					"compatible":["Coordinator", "TeamWorker", "Resource_Investigator"],
+					"conflict":["Plant", "Implementer", "Monitor"]
 				},
 
 				"Coordinator":{
-					"compatible":["Implementer", "Team Worker"],
+					"compatible":["Implementer", "TeamWorker"],
 					"conflict":[]
 				},
 
-				"Team Worker":{
-					"compatible":["Team Worker", "Implementer", "Plant"],
+				"TeamWorker":{
+					"compatible":["TeamWorker", "Implementer", "Plant"],
 					"conflict":["Shaper"]
 				},
 
-				"Monitor Evaluator":{
+				"Monitor":{
 					"compatible":["Implementer", "Coordinator"],
-					"conflict":["Completer Finisher", "Monitor Evaluator"]
+					"conflict":["Completer", "Monitor"]
 				},
 
 				"Shaper":{
-					"compatible":["Resource Investigator"],
+					"compatible":["Resource_Investigator"],
 					"conflict":[]
 				},
 
-				"Resource Investigator":{
-					"compatible":["Team Worker", "Implementer"],
-					"conflict":["Completer Finisher"]
+				"Resource_Investigator":{
+					"compatible":["TeamWorker", "Implementer"],
+					"conflict":["Completer"]
 				},
 
-				"Completer Finisher":{
+				"Completer":{
 					"compatible":["Implementer"],
-					"conflict":["Monitor Evaluator"]
+					"conflict":["Monitor"]
 				}
 			}
 
 	}';
 
-		$response = $_POST['data'];
+	$response_raw = $_POST['data'];
+	$response = array();
+	foreach ($response_raw as $key => $item) {
+	  if(isset($response[$response_raw[$key]["name"]])){
+	  	array_push($response[$response_raw[$key]["name"]], $response_raw[$key]["value"]);
+	  }else{
+	  	$response[$response_raw[$key]["name"]] = array($response_raw[$key]["value"]);
+	  }
+	}
+
+
+	$answers_entered = array(
+
+		"first" => array("pref" => $response["first_preference"][0], "com_con"=>array("com"=>$response["first_compatibility"], "con"=>$response["first_conflict"])),
+
+		"second" => array("pref" => $response["second_preference"][0], "com_con"=>array("com"=>$response["second_compatibility"], "con"=>$response["second_conflict"]))
+		);
+
+
+	$answer_key = json_decode($answer_key_string,true);
+
+	$status = array();
+
+	foreach ($answers_entered as $ind => $obj) {
+
+		$pref = $obj["pref"];
+
+		if($ind == "first"){
+
+			$status["first_compatibility"] = false;
+			$status["first_conflict"] = false;
+			if(!array_diff($obj["com_con"]["com"], $answer_key["roles"][$pref]["compatible"])){
+				$status["first_compatibility"] = true;
+			}
+			if(!array_diff($obj["com_con"]["con"], $answer_key["roles"][$pref]["conflict"])){
+				$status["first_conflict"] = true;
+			}
+		}
+
+		if($ind == "second"){
+
+			$status["second_compatibility"] = false;
+			$status["second_conflict"] = false;
+			if(!array_diff($obj["com_con"]["com"], $answer_key["roles"][$pref]["compatible"])){
+				$status["second_compatibility"] = true;
+			}
+			if(!array_diff($obj["com_con"]["con"], $answer_key["roles"][$pref]["conflict"])){
+				$status["second_conflict"] = true;
+			}
+		}
+
+
+	}
+
+	print_r($status);
+
+
+// $containsAllValues = !array_diff($obj["com"], $all);
+
+
 
 
 
 	echo '{
 		
 			"status":"we have an ok",
-			"response":"'.$response.'"
+			"response":"'.$response_raw.'"
 
 	}';
 
--- index.php
+++ index.php
@@ -3,28 +3,72 @@
 
 
 
+<style type="text/css">
 
+	.question_container{
+
+		margin-bottom:30px;
+	}
+
+	.input_container{
+
+		margin-left:2px;
+		margin-bottom:30px;
+
+	}
+
+	.input_container input{
+
+		margin-right: 10px;
+		margin-bottom:10px;
+	}
+
+
+	.input_container p{
+
+		font-weight: bold;
+
+	}
+
+	.preference_dropdown{
+
+		display: block;
+
+	}
+
+	.titles{
+		font-weight:bold;
+		font-size: 20px;
+
+	}
+
+	.hint_text{
+		color: #FFFFFF;
+		display: block;
+		padding:5px;
+		margin-top: 5px;
+		background-color: #FF6128;
+		text-align: center;
+	}
+
+</style>
 
 
 </head>
 <body>
 
-<h1>Welcome to the Teams101x Compatibilties and potential clashes activity</h1>
+
+
+<form id="survey_form" action="javascript:void(0);" method="POST">
 
 
 
-	
+<div class="question_container">
 
 
-	<form class="survey_form" action="javascript:void(0);" method="POST">
-
-
-
-		
-
-		<select name="preference_one" id="" class="preferences">
-			
-				
+	<div class="input_container preferences_inputs">
+		<p>What is your 1st preferred role type?</p>
+		<select name="first_preference" class="preference_dropdown target custom_select" id='preference_1'>
 				<option selected disabled hidden style='display: none' value=''></option>
 				<option value="Coordinator">Coordinator</option>
 				<option value="Shaper">Shaper</option>
@@ -34,16 +78,171 @@
 				<option value="Monitor">Monitor Evaluator</option>
 				<option value="Completer">Completer Finisher</option>
 				<option value="Resource_Investigator">Resource Investigator</option>
-
 		</select>
+	</div>
 
 
+	<div class="input_container preferences_inputs">
+		<p>Which peer role types are compatibile with your 1st preferred role type?</p>
+		<p>You can select as many as necessary.</p>
 
-		<button class="btn btn-primary btn-md makepdf">Submit</button> <button type='reset' class="btn btn-default btn-md resetButton">Reset</button>
-	</form>
+		<input type="checkbox" name="first_compatibility" id="first_compatibility_option_1" value="Coordinator">
+		Coordinator</br>
+
+		<input type="checkbox" name="first_compatibility" id="first_compatibility_option_2" value="Shaper">
+		Shaper</br>
+
+		<input type="checkbox" name="first_compatibility" id="first_compatibility_option_3" value="Implementer">
+		Implementer</br>
+
+		<input type="checkbox" name="first_compatibility" id="first_compatibility_option_4" value="Plant">
+		Plant</br>
+
+		<input type="checkbox" name="first_compatibility" id="first_compatibility_option_5" value="TeamWorker">
+		Team Worker</br>
+
+		<input type="checkbox" name="first_compatibility" id="first_compatibility_option_6" value="Monitor">
+		Monitor Evaluator</br>
+
+		<input type="checkbox" name="first_compatibility" id="first_compatibility_option_7" value="Completer">
+		Completer Finisher</br>
+
+		<input type="checkbox" name="first_compatibility" id="first_compatibility_option_8" value="Resource_Investigator">
+		Resource Investigator</br>
+
+		<input type="checkbox" name="first_compatibility" id="first_compatibility_option_9" value="none">
+		None of the above</br>
+
+	</div>
+
+	<div class="input_container preferences_inputs">
+		<p>Which peer role types are likely to clash with your 1st preferred role type?</p>
+		<p>You can select as many as necessary.</p>
+
+		<input type="checkbox" name="first_conflict" id="first_conflict_option_1" value="Coordinator">
+		Coordinator</br>
+
+		<input type="checkbox" name="first_conflict" id="first_conflict_option_2" value="Shaper">
+		Shaper</br>
+
+		<input type="checkbox" name="first_conflict" id="first_conflict_option_3" value="Implementer">
+		Implementer</br>
+
+		<input type="checkbox" name="first_conflict" id="first_conflict_option_4" value="Plant">
+		Plant</br>
+
+		<input type="checkbox" name="first_conflict" id="first_conflict_option_5" value="TeamWorker">
+		Team Worker</br>
+
+		<input type="checkbox" name="first_conflict" id="first_conflict_option_6" value="Monitor">
+		Monitor Evaluator</br>
+
+		<input type="checkbox" name="first_conflict" id="first_conflict_option_7" value="Completer">
+		Completer Finisher</br>
+
+		<input type="checkbox" name="first_conflict" id="first_conflict_option_8" value="Resource_Investigator">
+		Resource Investigator</br>
+
+		<input type="checkbox" name="first_conflict" id="first_conflict_option_9" value="none">
+		None of the above</br>
+
+	</div>
 
 
+</div>
 
+
+<div class="question_container">
+
+
+	<div class="input_container preferences_inputs">
+		<p>What is your 2nd preferred role type?</p>
+		<select name="second_preference" class="preference_dropdown target custom_select" id='preference_2'>
+				<option selected disabled hidden style='display: none' value=''></option>
+				<option value="Coordinator">Coordinator</option>
+				<option value="Shaper">Shaper</option>
+				<option value="Implementer">Implementer</option>
+				<option value="Plant">Plant</option>
+				<option value="TeamWorker">Team Worker</option>
+				<option value="Monitor">Monitor Evaluator</option>
+				<option value="Completer">Completer Finisher</option>
+				<option value="Resource_Investigator">Resource Investigator</option>
+		</select>
+	</div>
+
+
+	<div class="input_container preferences_inputs">
+		<p>Which peer role types are compatibile with your 2nd preferred role type?</p>
+		<p>You can select as many as necessary.</p>
+
+		<input type="checkbox" name="second_compatibility" id="second_compatibility_option_1" value="Coordinator">
+		Coordinator</br>
+
+		<input type="checkbox" name="second_compatibility" id="second_compatibility_option_2" value="Shaper">
+		Shaper</br>
+
+		<input type="checkbox" name="second_compatibility" id="second_compatibility_option_3" value="Implementer">
+		Implementer</br>
+
+		<input type="checkbox" name="second_compatibility" id="second_compatibility_option_4" value="Plant">
+		Plant</br>
+
+		<input type="checkbox" name="second_compatibility" id="second_compatibility_option_5" value="TeamWorker">
+		Team Worker</br>
+
+		<input type="checkbox" name="second_compatibility" id="second_compatibility_option_6" value="Monitor">
+		Monitor Evaluator</br>
+
+		<input type="checkbox" name="second_compatibility" id="second_compatibility_option_7" value="Completer">
+		Completer Finisher</br>
+
+		<input type="checkbox" name="second_compatibility" id="second_compatibility_option_8" value="Resource_Investigator">
+		Resource Investigator</br>
+
+		<input type="checkbox" name="second_compatibility" id="second_compatibility_option_9" value="none">
+		None of the above</br>
+
+	</div>
+
+	<div class="input_container preferences_inputs">
+		<p>Which peer role types are likely to clash with your 2nd preferred role type?</p>
+		<p>You can select as many as necessary.</p>
+
+		<input type="checkbox" name="second_conflict" id="second_conflict_option_1" value="Coordinator">
+		Coordinator</br>
+
+		<input type="checkbox" name="second_conflict" id="second_conflict_option_2" value="Shaper">
+		Shaper</br>
+
+		<input type="checkbox" name="second_conflict" id="second_conflict_option_3" value="Implementer">
+		Implementer</br>
+
+		<input type="checkbox" name="second_conflict" id="second_conflict_option_4" value="Plant">
+		Plant</br>
+
+		<input type="checkbox" name="second_conflict" id="second_conflict_option_5" value="TeamWorker">
+		Team Worker</br>
+
+		<input type="checkbox" name="second_conflict" id="second_conflict_option_6" value="Monitor">
+		Monitor Evaluator</br>
+
+		<input type="checkbox" name="second_conflict" id="second_conflict_option_7" value="Completer">
+		Completer Finisher</br>
+
+		<input type="checkbox" name="second_conflict" id="second_conflict_option_8" value="Resource_Investigator">
+		Resource Investigator</br>
+
+		<input type="checkbox" name="second_conflict" id="second_conflict_option_9" value="none">
+		None of the above</br>
+
+	</div>
+
+
+</div>
+
+
+<button class="btn btn-primary btn-md submit_button">Submit</button> <button type='reset' class="btn btn-default btn-md resetButton">Reset</button>
+</form>
 
 
 
@@ -51,35 +250,66 @@
 
 
 
-	var data = {'data':{}};
+	$(".submit_button").click(function(event) {
 
-	data['user_id'] = '<?php echo $lti->user_id(); ?>';
-	data['data'] = "WAAATT";
+		var form_data = $('#survey_form').serializeArray();
 
-	data['form'] = $('#questionnaire_form').serialize();
-
-	data['lti_id'] = '<?php echo $lti->lti_id(); ?>';
-	data['lis_outcome_service_url'] = '<?php echo $lti->grade_url(); ?>';
-	data['lis_result_sourcedid'] = '<?php echo $lti->result_sourcedid(); ?>';
+	
+		checkData(form_data);
 
 
-
-	$.ajax({
-	  type: "POST",
-	  url: "checkdata.php",
-	  data: data,
-	  success: function(response) {
-
-		 console.log(response);
-
-	  },
-	  error: function(error){
-
-		  console.log(error);
-	  }
 	});
 
 
+
+
+	function checkData(data_to_save){
+
+
+		var data = {'data':{}};
+
+		data['data'] = data_to_save;
+		data['user_id'] = '<?php echo $lti->user_id(); ?>';
+		data['lti_id'] = '<?php echo $lti->lti_id(); ?>';
+		data['lis_outcome_service_url'] = '<?php echo $lti->grade_url(); ?>';
+		data['lis_result_sourcedid'] = '<?php echo $lti->result_sourcedid(); ?>';
+
+
+		$.ajax({
+		  type: "POST",
+		  url: "checkdata.php",
+		  data: data,
+		  success: function(response) {
+			 console.log(response);
+		  },
+		  error: function(error){
+			  console.log(error);
+		  }
+		});
+
+
+	}
+
+	function displayStatus(status){
+
+		/**
+
+			first_compatibility_status:false;
+			first_conflict_status:true;
+
+			second_compatibility_status:false;
+			second_conflict_status:true;
+
+		*/
+
+
+	}
+
+	function saveData(data){
+
+
+
+	}
 
 
 
