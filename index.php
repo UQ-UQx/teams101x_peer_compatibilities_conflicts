6aaa1bf556b40a60cb272bb185b71de816cf24fd
Ankith Konda <a.konda@uq.edu.au>
2016-03-14 11:52:51 +1000

    Impliment save state

--- index.php
+++ index.php
@@ -1,6 +1,42 @@
 <?php require_once('inc/header.php'); ?>
 
+<?php
 
+	$sid = $lti->resource_id().'-lti-uid-'.$lti->user_id();
+
+	require_once('model.php');
+
+
+
+	$saved_data_raw = getResponse($sid);
+
+	$data_available = false;
+		$number_of_selections = 0;
+
+
+	if(isset($saved_data_raw)){
+
+
+
+		$saved_data_raw_json = json_decode($saved_data_raw->response);
+		$saved_data = array();
+		foreach ($saved_data_raw_json as $ind => $obj) {
+
+			if(isset($saved_data[$obj->name])){
+				array_push($saved_data[$obj->name], $obj->value);
+				
+			}else{
+				$saved_data[$obj->name] = array($obj->value);
+			}
+		}
+		// $saved_data_json = json_encode($saved_data);
+
+		$number_of_selections = count($saved_data);
+		$data_available = true;
+
+	}
+
+?>
 
 
 <style type="text/css">
@@ -74,8 +110,60 @@
 <body>
 
 
-
 <form id="survey_form" action="javascript:void(0);" method="POST">
+
+
+<?php
+
+
+
+	$number_of_options = 2;
+
+
+	$options_string = '{
+
+			"roles":{
+
+				"Implementer":{
+					"display_name":"Implementer"
+				},
+
+				"Plant":{
+					"display_name":"Plant"
+				},
+
+				"Coordinator":{
+					"display_name":"Coordinator"
+				},
+
+				"TeamWorker":{
+					"display_name":"Team Worker"
+				},
+
+				"Monitor":{
+					"display_name":"Monitor Evaluator"
+				},
+
+				"Shaper":{
+					"display_name":"Shaper"
+				},
+
+				"Resource_Investigator":{
+					"display_name":"Resource Investigator"
+				},
+
+				"Completer":{
+					"display_name":"Completer Finisher"
+				}
+			}
+	}';
+
+
+	$options = json_decode($options_string,true);
+
+?>
+
+
 
 
 
@@ -84,17 +172,36 @@
 
 	<div class="input_container preferences_inputs">
 		<p>What is your 1st preferred role type?</p>
-		<select name="first_preference" class="preference_dropdown target custom_select" id='preference_1'>
-				<option selected disabled hidden style='display: none' value=''></option>
-				<option value="Coordinator">Coordinator</option>
-				<option value="Shaper">Shaper</option>
-				<option value="Implementer">Implementer</option>
-				<option value="Plant">Plant</option>
-				<option value="TeamWorker">Team Worker</option>
-				<option value="Monitor">Monitor Evaluator</option>
-				<option value="Completer">Completer Finisher</option>
-				<option value="Resource_Investigator">Resource Investigator</option>
-		</select>
+
+
+		<?php
+
+
+
+
+			echo '<select name="first_preference" class="preference_dropdown target custom_select" id="preference_1"><option selected disabled hidden style="display: none" value=""></option>';
+			foreach ($options["roles"] as $value => $obj) {
+				if(isset($saved_data["first_preference"])){
+					if($saved_data["first_preference"][0] == $value){
+						echo '<option selected value='.$value.' >'.$obj["display_name"].'</option>';
+					}else{
+						echo '<option value='.$value.' >'.$obj["display_name"].'</option>';
+					}
+				}else{
+					echo '<option value='.$value.' >'.$obj["display_name"].'</option>';
+				}
+				//echo $value." --- ".$display_name["display_name"]."</br>";
+				//Implementer --- Implementer
+				// Plant --- Plant
+				// Coordinator --- Coordinator
+				// TeamWorker --- Team Worker
+				// Monitor --- Monitor Evaluator
+				// Shaper --- Shaper
+				// Resource_Investigator --- Resource Investigator
+				// Completer --- Completer Finisher
+			}
+			echo '</select>';
+		?>
 	</div>
 
 
@@ -102,29 +209,23 @@
 		<p>Which peer role types are compatibile with your 1st preferred role type?</p>
 		<p>You can select as many as necessary.</p>
 
-		<input type="checkbox" name="first_compatibility" id="first_compatibility_option_1" value="Coordinator" class="target">
-		Coordinator</br>
 
-		<input type="checkbox" name="first_compatibility" id="first_compatibility_option_2" value="Shaper" class="target">
-		Shaper</br>
+		<?php
 
-		<input type="checkbox" name="first_compatibility" id="first_compatibility_option_3" value="Implementer" class="target">
-		Implementer</br>
+			foreach ($options["roles"] as $value => $obj) {
+				if(isset($saved_data["first_compatibility"])){
 
-		<input type="checkbox" name="first_compatibility" id="first_compatibility_option_4" value="Plant" class="target">
-		Plant</br>
+					if(in_array($value,$saved_data["first_compatibility"])){
+						echo '<input type="checkbox" name="first_compatibility" value="'.$value.'" class="target" checked>'.$obj["display_name"].'</br>';
+					}else{
+						echo '<input type="checkbox" name="first_compatibility" value="'.$value.'" class="target">'.$obj["display_name"].'</br>';
+					}
+				}else{
+					echo '<input type="checkbox" name="first_compatibility" value="'.$value.'" class="target">'.$obj["display_name"].'</br>';
+				}
+			}
 
-		<input type="checkbox" name="first_compatibility" id="first_compatibility_option_5" value="TeamWorker" class="target">
-		Team Worker</br>
-
-		<input type="checkbox" name="first_compatibility" id="first_compatibility_option_6" value="Monitor" class="target">
-		Monitor Evaluator</br>
-
-		<input type="checkbox" name="first_compatibility" id="first_compatibility_option_7" value="Completer" class="target">
-		Completer Finisher</br>
-
-		<input type="checkbox" name="first_compatibility" id="first_compatibility_option_8" value="Resource_Investigator" class="target">
-		Resource Investigator</br>
+		?>
 
 		<i class="first_compatibility_status status"></i>
 
@@ -134,32 +235,39 @@
 		<p>Which peer role types are likely to clash with your 1st preferred role type?</p>
 		<p>You can select as many as necessary.</p>
 
-		<input type="checkbox" name="first_conflict" id="first_conflict_option_1" value="Coordinator" class="target">
-		Coordinator</br>
 
-		<input type="checkbox" name="first_conflict" id="first_conflict_option_2" value="Shaper" class="target">
-		Shaper</br>
 
-		<input type="checkbox" name="first_conflict" id="first_conflict_option_3" value="Implementer" class="target">
-		Implementer</br>
+		<?php
 
-		<input type="checkbox" name="first_conflict" id="first_conflict_option_4" value="Plant" class="target">
-		Plant</br>
+			foreach ($options["roles"] as $value => $obj) {
+				if(isset($saved_data["first_conflict"])){
 
-		<input type="checkbox" name="first_conflict" id="first_conflict_option_5" value="TeamWorker" class="target">
-		Team Worker</br>
+					if(in_array($value,$saved_data["first_conflict"])){
+						echo '<input type="checkbox" name="first_conflict" value="'.$value.'" class="target" checked>'.$obj["display_name"].'</br>';
+					}else{
+						echo '<input type="checkbox" name="first_conflict" value="'.$value.'" class="target">'.$obj["display_name"].'</br>';
+					}
+				}else{
+					echo '<input type="checkbox" name="first_conflict" value="'.$value.'" class="target">'.$obj["display_name"].'</br>';
+				}
+			}
 
-		<input type="checkbox" name="first_conflict" id="first_conflict_option_6" value="Monitor" class="target">
-		Monitor Evaluator</br>
 
-		<input type="checkbox" name="first_conflict" id="first_conflict_option_7" value="Completer" class="target">
-		Completer Finisher</br>
+			if(isset($saved_data["first_conflict"])){
 
-		<input type="checkbox" name="first_conflict" id="first_conflict_option_8" value="Resource_Investigator" class="target">
-		Resource Investigator</br>
+					if(in_array("none",$saved_data["first_conflict"])){
+						echo '	<input type="checkbox" name="first_conflict" value="none" class="target" checked>
+		No increased likelihood of conflict between this role and any other particular role</br>';
+					}else{
+						echo '	<input type="checkbox" name="first_conflict" value="none" class="target">
+		No increased likelihood of conflict between this role and any other particular role</br>';
+							}
+				}else{
+					echo '	<input type="checkbox" name="first_conflict" value="none" class="target">
+		No increased likelihood of conflict between this role and any other particular role</br>';
+				}
+		?>
 
-		<input type="checkbox" name="first_conflict" id="first_conflict_option_9" value="none" class="target">
-		No increased likelihood of conflict between this role and any other particular role</br>
 
 		<i class="first_conflict_status status"></i>
 
@@ -174,17 +282,22 @@
 
 	<div class="input_container preferences_inputs">
 		<p>What is your 2nd preferred role type?</p>
-		<select name="second_preference" class="preference_dropdown target custom_select" id='preference_2'>
-				<option selected disabled hidden style='display: none' value=''></option>
-				<option value="Coordinator">Coordinator</option>
-				<option value="Shaper">Shaper</option>
-				<option value="Implementer">Implementer</option>
-				<option value="Plant">Plant</option>
-				<option value="TeamWorker">Team Worker</option>
-				<option value="Monitor">Monitor Evaluator</option>
-				<option value="Completer">Completer Finisher</option>
-				<option value="Resource_Investigator">Resource Investigator</option>
-		</select>
+		<?php
+			echo '<select name="second_preference" class="preference_dropdown target custom_select" id="preference_2"><option selected disabled hidden style="display: none" value=""></option>';
+			foreach ($options["roles"] as $value => $obj) {
+				if(isset($saved_data["second_preference"])){
+
+					if($saved_data["second_preference"][0] == $value){
+						echo '<option selected value='.$value.' >'.$obj["display_name"].'</option>';
+					}else{
+						echo '<option value='.$value.' >'.$obj["display_name"].'</option>';
+					}
+				}else{
+					echo '<option value='.$value.' >'.$obj["display_name"].'</option>';
+				}
+			}
+			echo '</select>';
+		?>
 	</div>
 
 
@@ -192,29 +305,22 @@
 		<p>Which peer role types are compatibile with your 2nd preferred role type?</p>
 		<p>You can select as many as necessary.</p>
 
-		<input type="checkbox" name="second_compatibility" id="second_compatibility_option_1" value="Coordinator" class="target">
-		Coordinator</br>
+		<?php
 
-		<input type="checkbox" name="second_compatibility" id="second_compatibility_option_2" value="Shaper" class="target">
-		Shaper</br>
+			foreach ($options["roles"] as $value => $obj) {
+				if(isset($saved_data["second_compatibility"])){
 
-		<input type="checkbox" name="second_compatibility" id="second_compatibility_option_3" value="Implementer" class="target">
-		Implementer</br>
+					if(in_array($value,$saved_data["second_compatibility"])){
+						echo '<input type="checkbox" name="second_compatibility" value="'.$value.'" class="target" checked>'.$obj["display_name"].'</br>';
+					}else{
+						echo '<input type="checkbox" name="second_compatibility" value="'.$value.'" class="target">'.$obj["display_name"].'</br>';
+					}
+				}else{
+					echo '<input type="checkbox" name="second_compatibility" value="'.$value.'" class="target">'.$obj["display_name"].'</br>';
+				}
+			}
 
-		<input type="checkbox" name="second_compatibility" id="second_compatibility_option_4" value="Plant" class="target">
-		Plant</br>
-
-		<input type="checkbox" name="second_compatibility" id="second_compatibility_option_5" value="TeamWorker" class="target">
-		Team Worker</br>
-
-		<input type="checkbox" name="second_compatibility" id="second_compatibility_option_6" value="Monitor" class="target">
-		Monitor Evaluator</br>
-
-		<input type="checkbox" name="second_compatibility" id="second_compatibility_option_7" value="Completer" class="target">
-		Completer Finisher</br>
-
-		<input type="checkbox" name="second_compatibility" id="second_compatibility_option_8" value="Resource_Investigator" class="target">
-		Resource Investigator</br>
+		?>
 
 		<i class="second_compatibility_status status"></i>
 
@@ -225,32 +331,39 @@
 		<p>Which peer role types are likely to clash with your 2nd preferred role type?</p>
 		<p>You can select as many as necessary.</p>
 
-		<input type="checkbox" name="second_conflict" id="second_conflict_option_1" value="Coordinator" class="target">
-		Coordinator</br>
 
-		<input type="checkbox" name="second_conflict" id="second_conflict_option_2" value="Shaper" class="target">
-		Shaper</br>
+		<?php
 
-		<input type="checkbox" name="second_conflict" id="second_conflict_option_3" value="Implementer" class="target">
-		Implementer</br>
+			foreach ($options["roles"] as $value => $obj) {
+				if(isset($saved_data["second_conflict"])){
 
-		<input type="checkbox" name="second_conflict" id="second_conflict_option_4" value="Plant" class="target">
-		Plant</br>
+					if(in_array($value,$saved_data["second_conflict"])){
+						echo '<input type="checkbox" name="second_conflict" value="'.$value.'" class="target" checked>'.$obj["display_name"].'</br>';
+					}else{
+						echo '<input type="checkbox" name="second_conflict" value="'.$value.'" class="target">'.$obj["display_name"].'</br>';
+					}
+				}else{
+					echo '<input type="checkbox" name="second_conflict" value="'.$value.'" class="target">'.$obj["display_name"].'</br>';
+				}
+			}
 
-		<input type="checkbox" name="second_conflict" id="second_conflict_option_5" value="TeamWorker" class="target">
-		Team Worker</br>
 
-		<input type="checkbox" name="second_conflict" id="second_conflict_option_6" value="Monitor" class="target">
-		Monitor Evaluator</br>
+			if(isset($saved_data["second_conflict"])){
+					if(in_array("none",$saved_data["second_conflict"])){
+						echo '	<input type="checkbox" name="second_conflict" value="none" class="target" checked>
+		No increased likelihood of conflict between this role and any other particular role</br>';
+					}else{
+						echo '	<input type="checkbox" name="second_conflict" value="none" class="target">
+		No increased likelihood of conflict between this role and any other particular role</br>';
+							}
+			}else{
+					echo '	<input type="checkbox" name="second_conflict" value="none" class="target">
+		No increased likelihood of conflict between this role and any other particular role</br>';
+			}
+		?>
 
-		<input type="checkbox" name="second_conflict" id="second_conflict_option_7" value="Completer" class="target">
-		Completer Finisher</br>
 
-		<input type="checkbox" name="second_conflict" id="second_conflict_option_8" value="Resource_Investigator" class="target">
-		Resource Investigator</br>
 
-		<input type="checkbox" name="second_conflict" id="second_conflict_option_9" value="none" class="target">
-		No increased likelihood of conflict between this role and any other particular role</br>
 
 		<i class="second_conflict_status status"></i>
 
@@ -269,15 +382,26 @@
 
 <script type="text/javascript">$(document).ready(function(){
 
-	toggleGenerateButton("off");
+					$(".hint_text").hide();
 
+
+	var saved_data_amount =  '<?php echo $number_of_selections;  ?>'
+	console.log(saved_data_amount);
+
+	if(saved_data_amount == 6){
+		toggleGenerateButton("on");
+
+
+	}else{
+		toggleGenerateButton("off");
+
+	}
 
 	$(".submit_button").click(function(event) {
 
-		$(".resetButton").addClass('disabled');
 		$(".resetButton").prop("disabled", true);
 
-		$(this).addClass('disabled');
+
 		$(this).prop("disabled", true);
 		$(this).empty().append("Submitting <i class='fa fa-spinner fa-pulse'></i>");
 
@@ -289,16 +413,32 @@
 
 	$(".resetButton").click(function(evemt){
 
-		$(".submit_button").addClass('disabled');
-		$(".submit_button").prop("disabled", true);
+		toggleGenerateButton("off");
 
-		$(this).addClass('disabled');
+
 		$(this).prop("disabled", true);
  		$(this).empty().append("Resetting <i class='fa fa-spinner fa-pulse'></i>");
 
-		$('#survey_form')[0].reset();
-		var form_data = $('#survey_form').serializeArray();
-		checkData(form_data);
+		checkData();
+
+		//$('#survey_form')[0].reset();
+
+
+		$('#survey_form').trigger("reset");
+
+		$('.target').removeAttr("checked");
+		$('option:selected', 'select[name="first_preference"]').removeAttr('selected');
+		$('option:selected', 'select[name="second_preference"]').removeAttr('selected');
+
+		$('select[name="first_preference"]').find('option[value=""]').attr("selected",true);
+		$('select[name="second_preference"]').find('option[value=""]').attr("selected",true);
+
+
+
+
+		var form_data_to_save = JSON.stringify($('#survey_form').serializeArray());
+		saveData(form_data_to_save);
+
 
 		$('.status').removeClass("fa fa-times fa-check");
 
@@ -309,13 +449,16 @@
 	$('.target').change(function(event){
 
 		var form_data = $('#survey_form').serializeArray();
+		var form_data_to_save = $('#survey_form').serializeArray();
+
+
+		form_data_to_save = JSON.stringify(form_data_to_save);
 
 		var formated_data = {};
 
 		$.each(form_data,function(ind, obj){
 
 			if(!(obj["name"] in formated_data)){
-
 				formated_data[obj["name"]] = [obj["value"]];
 			}else{
 				formated_data[obj["name"]].push(obj["value"]);
@@ -324,7 +467,7 @@
 		});
 
 
-		saveData(form_data);
+		saveData(form_data_to_save);
 
 
 		if(Object.keys(formated_data).length == 6){
@@ -349,6 +492,8 @@
 		data['lti_id'] = '<?php echo $lti->lti_id(); ?>';
 		data['lis_outcome_service_url'] = '<?php echo $lti->grade_url(); ?>';
 		data['lis_result_sourcedid'] = '<?php echo $lti->result_sourcedid(); ?>';
+		data['sid'] = '<?php echo $sid ?>';
+
 
 		$.ajax({
 		  type: "POST",
@@ -369,9 +514,6 @@
 
 			 }else{
 
-				$(".submit_button").removeClass('disabled');
-				$(".submit_button").prop("disabled", false);
-
 			 	$(".resetButton").removeClass('disabled');
 				$(".resetButton").prop("disabled", false);
  				$(".resetButton").empty().append("Reset");
@@ -388,7 +530,32 @@
 	}
 
 
-	function saveData(data){
+	function saveData(data_to_save){
+
+
+
+		var data = {'data':{}};
+
+		data['data'] = data_to_save;
+		data['user_id'] = '<?php echo $lti->user_id(); ?>';
+		data['lti_id'] = '<?php echo $lti->lti_id(); ?>';
+		data['lis_outcome_service_url'] = '<?php echo $lti->grade_url(); ?>';
+		data['lis_result_sourcedid'] = '<?php echo $lti->result_sourcedid(); ?>';
+		data['sid'] = '<?php echo $sid ?>';
+
+
+		$.ajax({
+		  type: "POST",
+		  url: "savedata.php",
+		  data: data,
+		  success: function(response) {
+			 console.log(response);
+		  },
+		  error: function(error){
+			  console.log(error);
+		  }
+		});
+
 
 
 
@@ -401,9 +568,12 @@
 
 				if(val == false){
 
+					$("."+key).removeClass("fa fa-check");
+
 					$("."+key).addClass("fa fa-times");
 
 				}else{
+					$("."+key).removeClass("fa fa-times");
 
 					$("."+key).addClass("fa fa-check");
 
